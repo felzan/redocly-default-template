7aa89cd66d1c4dee0eb574b22b907b7036d687a3
Roman Hotsiy <gotsijroman@gmail.com>
2018-02-28 14:56:20 +0200

    update ga code

--- content/scripts.tsx
+++ content/scripts.tsx
@@ -1,19 +1,21 @@
 import * as React from 'react';
 
-const gaTrackingCode = 'gaTrackingCode';
+const gaTrackingCode = '';
 
 export default (
   <React.Fragment>
-    <script
-      dangerouslySetInnerHTML={{
-        __html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    {gaTrackingCode ? (
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
 ga('create', '${gaTrackingCode}', 'auto');
 ga('send', 'pageview');`,
-      }}
-    />
+        }}
+      />
+    ) : null}
   </React.Fragment>
 );
